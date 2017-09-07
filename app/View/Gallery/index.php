6d0068b1d2ebdf7145037f46cc55c809be3da6c7
Mark Endrey <ungazik@gmail.com>
2017-09-07 09:16:21 +0200

    Minor change: typos

--- app/View/Gallery/index.php
+++ app/View/Gallery/index.php
@@ -4,5 +4,5 @@
 		</ul>
 		<h1><?php echo $title; ?></h1>
 		<p>The first menu item &ldquo;<a href="/samples">Email link samples</a>&rdquo; leads You to link samples of a possible email offer. That is where the real web application can be demonstrated.</p>
-		<p>The other menu item &ldquo;<a href="/dompag">Simplified JavaScript pagination</a>&hellip;&rdquo; presents how pagination can be solved without repeeated page reload. The JavaScript technique is exemplified on an extremely simplified sample: a simple ectual list is &ldquo;scrolled&rdquo; up and down.</p>
+		<p>The other menu item &ldquo;<a href="/dompag">Simplified JavaScript pagination</a>&hellip;&rdquo; presents how pagination can be solved without repeated page reload. The JavaScript technique is exemplified on an extremely simplified sample: a simple textual list is &ldquo;scrolled&rdquo; up and down.</p>
 
--- app/View/Gallery/samples.php
+++ app/View/Gallery/samples.php
@@ -3,7 +3,7 @@
 		</ul>
 		<h1><?php echo $title; ?></h1>
 		<p>The gallery exists in two versions. The &ldquo;plain&rdquo; versions use a primitive but very stable technique. The whole page (including all images) gets reloaded during pagination. It is not very user-friendly, but at least it is free of bugs due to its simplicity. Another advantage of this ancient technique is that it works in any kind of browsers, even in old ones, and even if the user disables JavaScript intentionally or accidentally.</p>
-		<p>The other version (labeled here as &ldquo;js&rdquo;) is more resource-efficient, quicker, user-friendlier. During pagination, only the images move and chonge size, but nothing gets reloaded. This provides a better visual effect, but this technique is prone to errors and bugs. Now this version is buggy yet, some samples work, while others are still wrong. Even when the bugs get fixed, this techique is very sensitive to user settings and browser versions.</p>
+		<p>The other version (labeled here as &ldquo;JavaScript&rdquo; version or &ldquo;js&rdquo; in short) is more resource-efficient, quicker, user-friendlier. During pagination, only the images move and chonge size, but nothing gets reloaded. This provides a better visual effect, but this technique is prone to errors and bugs. Now this version is buggy yet, some samples work, while others are still wrong. Even when the bugs get fixed, this techique is very sensitive to user settings and browser versions.</p>
 		Offer #1 focusing:
 		<ul>
 			<li>picture #1:  (<a href="/focus/1/1">plain</a>)  (<a href="/focus-js/1/1">js</a>)</li>
