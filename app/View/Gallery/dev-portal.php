6773103958467231d8e458dd07836401bdd22fcd
Mark Endrey <ungazik@gmail.com>
2017-09-12 09:14:18 +0200

    Add frontend design to development protal

--- app/View/Gallery/dev-portal.php
+++ app/View/Gallery/dev-portal.php
@@ -1,7 +1,8 @@
 		<ul class="upper-menu internal">
-			<li><a href="/samples">Email link samples</a></li>
-			<li><a class="menu-icon" href="/dompag">Simplified JavaScript pagination with a simple textual list</a></li>
-			<li><a href="/home">Home for the final app &mdash; a redirect</a></li>
+			<li><a href="/samples"title="Mimicking sent-out offer emails">Email link samples</a></li>
+			<li><a class="menu-icon" href="/dompag" title="Simplified JavaScript pagination with a simple textual list">Simplified JS-pagination</a></li>
+			<li><a href="/home" title="Home for the final app &mdash; a redirect">Final home</a></li>
+			<li><a href="/frontend-design/index.html" title="Frontend design &mdash; to be integrated into the final app">Frontend design</a></li>
 		</ul>
 		<h1><?php echo $title; ?></h1>
 		<p>This is a portal for development purposes only. The final webapp will have the following <a href="/home">redirect</a> as its home.</p>
